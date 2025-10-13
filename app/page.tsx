84b5f1cdf5467c21078143d1648f69bf6997cf1a
Passang Dorji <pssngdor94@gmail.com>
2025-10-13 11:14:27 +0600

    fix: fixes logo and landing page

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,6 @@ import Features from "./component/Features";
 import Testimonials from "./component/Review";
 import ContactPage from "./component/Contact";
 export default function Home() {
-	// return (
-	// 	<div>
-	// 		<HomePage />
-	// 		<AboutPage />
-	// 		<Destinations />
-	// 		<Features />
-	// 		<Testimonials />
-	// 		<ContactPage />
-	// 	</div>
-	// );
 	return (
 		<div>
 			<section id="home">
